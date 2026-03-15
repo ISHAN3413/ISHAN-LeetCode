d6eae859fd43c3795ca2426af7865850dc7b3b86
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-03-15 10:59:25 +0530

    Time: 3 ms (27.38%), Space: 14.9 MB (12.06%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> a;
+        std::unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto i:m){
+            if(i.second==1)a.push_back(i.first);
+        }
+        return a;
+    }
+};
