c09334470acd8f562b9cb18a64c1546ae47871fb
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-03-24 20:08:39 +0530

    Time: 0 ms (100%), Space: 9.9 MB (38.14%) - LeetHub

---
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& pos) {
+        int odd = 0,even =0;
+        for(int i =0;i<pos.size();i++){
+            if(pos[i]%2 == 0)even++;
+            else odd++;
+        }
+        return min(odd,even);
+    }
+};
