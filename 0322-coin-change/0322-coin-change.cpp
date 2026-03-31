7bbfe0d0bddd8d78e2d1ba67171ff457be730863
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-03-31 13:04:32 +0530

    Time: 46 ms (34.78%), Space: 52.9 MB (14.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount){
+    int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1 , vector<int>(amount+1 , -1));
-        int ans =  f(coins , amount , 0 ,dp);
-        if(ans == INT_MAX)return -1;
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX));
+
+        for (int i = 0; i < dp.size(); i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int am = 0; am <= amount; am++) {
+                int pick = INT_MAX;
+                if (coins[i] <= am) {
+                    int res = dp[i][am-coins[i]];
+                    if (res != INT_MAX)
+                        pick = 1 + res;
+                }
+                int notpick = dp[i+1][am];
+               dp[i][am] = min(pick, notpick);
+            }
+        }
+        int ans = dp[0][amount];
+        if (ans == INT_MAX)
+            return -1;
         return ans;
     }
-    int f(vector<int> &coins , int am , int i , vector<vector<int>> &dp){
-        if(am <= 0)return 0;
-        if(i >= coins.size())return INT_MAX;
-        if(dp[i][am]!=-1)return dp[i][am];
+    int f(vector<int>& coins, int am, int i, vector<vector<int>>& dp) {
+        if (am <= 0)
+            return 0;
+        if (i >= coins.size())
+            return INT_MAX;
+        if (dp[i][am] != -1)
+            return dp[i][am];
         int pick = INT_MAX;
-        if(coins[i]<=am){
-            int res =  f(coins , am-coins[i] , i ,dp);
-            if(res!=INT_MAX)pick = 1+res;
+        if (coins[i] <= am) {
+            int res = f(coins, am - coins[i], i, dp);
+            if (res != INT_MAX)
+                pick = 1 + res;
         }
-        int notpick = f(coins , am , i+1 ,dp);
-        return dp[i][am] = min(pick , notpick);
+        int notpick = f(coins, am, i + 1, dp);
+        return dp[i][am] = min(pick, notpick);
     }
 };
