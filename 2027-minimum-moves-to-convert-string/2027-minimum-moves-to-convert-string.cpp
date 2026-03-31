7e81cd32b27bfe091e18b069d14eb7b72b7b2264
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-03-31 18:25:59 +0530

    Time: 0 ms (100%), Space: 8.8 MB (4.08%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int ans = 0;
+        int i;
+        for( i = 0 ;i<s.length();i++){
+            if(s[i] == 'X'){
+                ans++;
+                i+=2;
+            }
+        }
+        return ans;
+    }
+};
