6cffa89ae4642102155b6f86a68e02a684afe5c6
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-04-09 19:40:42 +0530

    Time: 0 ms (100%), Space: 8 MB (56.16%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int d = 2;
+        int ans = 0;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans;
+    }
+};
