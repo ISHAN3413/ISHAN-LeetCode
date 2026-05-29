b4100f754f0209ffb9b87d847c9c53d31f30e834
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-05-29 22:19:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.49%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1 , -1);
+        return f(n,dp);
+    }
+    int f(int n , vector<int> &dp){
+        if(n==2)return 1;
+        if(dp[n]!=-1)return dp[n];
+        int ans = 0;
+       
+        for(int i = 1 ; i<n;i++){
+            ans=max(ans , max(i*(n-i) , i*f(n-i , dp)));
+        }
+        return dp[n]=ans;
+    }
+};
