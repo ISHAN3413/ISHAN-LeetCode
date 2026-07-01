795e1684d35a71e59e7b72e7eea4e77a14f64cb2
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-07-01 17:51:55 +0530

    Time: 11 ms (57.27%), Space: 26 MB (22.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+    int offset;
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums,0,target,0);
+        int n = nums.size();
+        int sum = 0;
+        for(int s:nums)sum+=s;
+        offset = sum;
+        vector<vector<int>> dp(n,vector<int>(2*sum +1 , -1));
+        return f(nums,0,target,0 ,dp);
     }
-    int f(vector<int>& nums , int i,int target,int curr){
+    int f(vector<int>& nums , int i,int target,int curr,vector<vector<int>>&dp){
         if( i>=nums.size()){
             if(target==curr)return 1;
             else return 0;
         }
+        if(dp[i][curr+offset]!=-1)return dp[i][curr+offset];
         int ans = 0;
-        ans+=f(nums,i+1,target,curr-nums[i]);
-        ans+=+f(nums,i+1,target,curr+nums[i]);
-        return ans;
+        ans+=f(nums,i+1,target,curr-nums[i],dp);
+        ans+=+f(nums,i+1,target,curr+nums[i],dp);
+        return dp[i][curr+offset] = ans;
     }
 };
