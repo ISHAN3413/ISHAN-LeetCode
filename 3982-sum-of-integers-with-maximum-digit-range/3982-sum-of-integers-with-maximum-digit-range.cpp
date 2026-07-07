9e8a204051c1064d80e9889b1bc2f21a6d342d71
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-07-07 16:22:58 +0530

    Time: 0 ms (100%), Space: 31.6 MB (33.93%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.cpp
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int maxDigitRange(vector<int>& nums) {
-        int ranmaxi = 0;
         int ans = 0;
+        int rmax = 0;
         for(int i = 0;i<nums.size();i++){
             string s = to_string(nums[i]);
+            int range = 0;
+            
             char maxi = s[0];
             char mini = s[0];
-            for(int j = 0;j<s.length();j++){
-                if(s[j]>maxi)maxi = s[j];
-                if(s[j]<mini)mini = s[j];
+            for(int j = 0; j <s.length();j++){
+                if(s[j]>maxi)maxi=s[j];
+                if(s[j]<mini)mini=s[j];
+                
             }
-            int ran = maxi -mini;
-            
-            if(ranmaxi == ran)ans+=nums[i];
-            else if(ran>ranmaxi) ans=nums[i];
-
-
-            ranmaxi = max(ranmaxi , ran);  
+            range = maxi-mini;
+            if(range>rmax){
+                ans = 0;
+                rmax = range;
+            }
+            if(range == rmax)ans+=nums[i];
         }
         return ans;
     }
