6b54abe2faad832c9260305ebdb71baf8ced83e4
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-07-07 16:54:22 +0530

    Time: 9 ms (89.83%), Space: 26.4 MB (82.42%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s, string t) {
-        if(t.length()<s.length())return false;
-
-        if(t.length()==s.length()){
-            int cnt = 0;
-            for(int i=0;i<s.length();i++){
-                if(s[i]!=t[i])cnt++;
-                if(cnt>1)return false;
+        if(s.length()>t.length())return false;
+        if(s.length()==t.length()){
+            int j=0;
+            bool cnt = false;
+            while(j<t.length()){
+                if(s[j]==t[j])j++;
+                else {
+                    if(!cnt){
+                        cnt = true;
+                        j++;
+                    }
+                    else{
+                        return false;
+                    }
+                }
             }
         }
+        //vector<vector<vector<int>>>dp(s.length(),vector<vector<int>>(t.length(),vector<int>(3 , -1)));
         return f(s,t,0,0,0);
     }
+
     bool f(string &s , string &t , int i , int j , int cnt){
-        if(i>=s.length() || j>=t.length()){
-            if(j==t.length() && i<s.length()){
-                return false;
-            }
+        if(i>=s.length()){
             return true;
         }
-if(s[i]!=t[j] && cnt == 1){
+        if(j>=t.length()){
+            return false;
+        }
+        // if(dp[i][j][cnt]!=-1)return dp[i][j][cnt];
+        if(s[i]!=t[j] && cnt == 1){
         return false;
     }
-
-    bool temp;
-
-    if(s[i]==t[j]){
-        temp = f(s,t,i+1,j+1,cnt);
+        bool ans = false;
+        if(s[i]==t[j]){
+            ans=f(s,t,i+1,j+1,cnt);
+        }
+        else{
+            if(cnt == 1){
+                ans=f(s,t,i,j+1,cnt);
+            }
+            else{
+                ans=(f(s,t,i+1,j+1,cnt+1) || f(s,t,i,j+1,cnt));
+            } 
+        }
+        return ans;
     }
-    else{
-        temp = (f(s,t,i+1,j+1,1)||f(s,t,i,j+1,cnt));
-    }
-
-    return temp;
-
-
-    }
-
-    
 };
