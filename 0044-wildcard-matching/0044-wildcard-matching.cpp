c2527ddc460c7ec46c7921a4083f2fe649a77743
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-07-10 17:47:35 +0530

    Time: 181 ms (5.09%), Space: 54.9 MB (9.33%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int i = s.length();
-        int j = p.length();
-        vector<vector<int>> dp(i , vector<int>(j , -1));
-        return f(i-1 , j-1 , s, p , dp);
+        int n = s.length();
+        int m = p.length();
+        vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
+        return f(s,p,0,0,dp);
     }
-    bool f(int i , int j , const string &s ,const string &p , vector<vector<int>> &dp){
-        if(i<0 && j<0)return true;
-        if(i<0){
-            for(int k = j;k>=0 ; k--){
-                if(p[k]!='*')return false;
+    bool f(string &s , string &p , int i , int j,vector<vector<int>> &dp){
+        if(i>=s.length()){
+            while(j<p.length()){
+                if(p[j]!='*')return false;
+                j++;
             }
             return true;
         }
-        if(j<0){
+        if(j>=p.length()){
             return false;
         }
-        if(dp[i][j] != -1)return dp[i][j];
-        if((s[i] == p[j] )||( p[j] == '?')){
-            return dp[i][j] =  f(i-1 , j-1 , s , p , dp);
+        if(i==s.length() &&j==p.length())return true;
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        if(s[i] == p[j] || p[j] == '?'){
+            return dp[i][j] =  f(s,p,i+1,j+1,dp);
         }
-        if(p[j] == '*'){
-            return dp[i][j] =   (f(i-1 , j , s , p,dp)||f(i , j-1 , s , p,dp));
+        if(p[j]=='*'){
+            return dp[i][j] =  f(s,p,i+1,j,dp)||f(s,p,i,j+1,dp)||f(s,p,i+1,j+1,dp);
         }
-        return dp[i][j] =  false;
+        return dp[i][j] = false;
     }
 };
