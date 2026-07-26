451727aff4366a77a41ece51613c8e2e796cb973
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-07-26 17:09:39 +0530

    Time: 9 ms (47.46%), Space: 19.3 MB (72.1%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -3,30 +3,31 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int>height(m,0);
-        int ans = 0;
-        for(int i = 0;i<n ;i++){
-            for(int j = 0;j<m;j++){
-                if(matrix[i][j]=='1')height[j]++;
-                else height[j] = 0;
-            }
-            int maxa = l(height);
-            ans = max(ans , maxa);
-        }
-        return ans;
-    }
-    int l(vector<int>& heights) {
-        int n = heights.size();
-        stack<int> st;
+        vector<int> histo(m,0);
         int maxa = 0;
-        for(int i = 0;i<=n ;i++){
-            while(!st.empty()&&(i==n || heights[st.top()]>=heights[i])){
-                int height = heights[st.top()];
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1')histo[j]++;
+                else histo[j]=0;
+            }
+            maxa = max(maxa , f(histo));
+        }
+        return maxa;
+    }
+
+    int f(vector<int>&histo){
+        int maxa = 0;
+        int n = histo.size();
+        stack<int> st;
+        for(int i = 0;i<=n;i++){
+            while(!st.empty() &&(i==n || histo[st.top()]>histo[i])){
+                int height = histo[st.top()];
                 st.pop();
                 int width;
-                if(st.empty())width = i;
-                else width = i - st.top()-1;
-                maxa = max(maxa , width*height);
+                if(st.empty())width=i;
+                else width = i-st.top()-1;
+
+                maxa = max(maxa,height*width);
             }
             st.push(i);
         }
