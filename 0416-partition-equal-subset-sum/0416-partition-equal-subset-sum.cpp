e7b238b257108b25b5f6c34860ba17d6ccda1e61
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-07-30 16:32:46 +0530

    Time: 85 ms (77.62%), Space: 128.4 MB (11.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    bool h(int n, int k, vector<int>& arr) {
-        vector<bool> prev(k + 1, false), curr(k + 1, false);
-        prev[0] = true;
-        if (arr[0] <= k) prev[arr[0]] = true;
-
-        for (int i = 1; i < n; i++) {
-            curr[0] = true;
-            for (int target = 1; target <= k; target++) {
-                bool notTake = prev[target];
-                bool take = false;
-                if (arr[i] <= target) take = prev[target - arr[i]];
-                curr[target] = take || notTake;
-            }
-            prev = curr;
-        }
-        return prev[k];
-    }
-
     bool canPartition(vector<int>& nums) {
-        int tos = 0;
-        for (int num : nums) tos += num;
-        if (tos % 2 != 0) return false;
-        return h(nums.size(), tos / 2, nums);
+        int sum = 0;
+        for(int i:nums)sum+=i;
+        if(sum%2 !=0)return false;
+        sum=sum/2;
+        vector<vector<int>> dp(sum+1,vector<int>(nums.size()+1,-1));
+        return f(nums,sum,0,dp);
+    }
+    bool f(vector<int>&nums , int n , int i,vector<vector<int>>&dp){
+        if(n==0)return true; 
+        if(i>=nums.size()||n<0)return false;
+        
+        if(dp[n][i]!=-1)return dp[n][i];
+       
+        
+        return dp[n][i] = (f(nums,n-nums[i],i+1,dp) || f(nums,n,i+1,dp));
     }
 };
