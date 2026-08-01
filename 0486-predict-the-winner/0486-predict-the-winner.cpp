6f1db7071c38ee25329ff7fdfd9a43d2bca0f183
ISHAN AGARWAL <ishanagarwal3413@gmail.com>
2026-08-01 09:47:35 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
-        return f(nums,0,nums.size()-1)>=0;
+        int n = nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return f(nums,0,nums.size()-1,dp)>=0;
     }
-    long f(vector<int>&nums , int i , int j){
+    long f(vector<int>&nums , int i , int j , vector<vector<int>>&dp){
         if(i==j)return nums[i];
-        long a = nums[i]-f(nums,i+1,j);
-        long b = nums[j]-f(nums,i,j-1);
-        return max(a,b);
+        if(dp[i][j]!=-1)return dp[i][j];
+        long a = nums[i]-f(nums,i+1,j,dp);
+        long b = nums[j]-f(nums,i,j-1,dp);
+        return dp[i][j] = max(a,b);
     }
 };
